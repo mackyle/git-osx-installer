2cd3b6f254b78263aea019985fc12704254c96be
Kyle J. McKay <mackyle@gmail.com>
2014-09-20 21:47:48 -0700

    Add gettext implementation
    It's there and probably works based on the test program.

---
+++ src/build-prefix.h
@@ -0,0 +1,50 @@
+/*
+
+build-prefix.h - build prefix header for building git
+Copyright (C) 2014 Kyle J. McKay.  All rights reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
+#include <AvailabilityMacros.h>
+#ifndef MAC_OS_X_VERSION_10_4
+#define MAC_OS_X_VERSION_10_4 1040
+#endif
+#ifndef MAC_OS_X_VERSION_10_5
+#define MAC_OS_X_VERSION_10_5 1050
+#endif
+#undef OLD_ICONV
+#undef XDL_FAST_HASH
+#if MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_5
+#define OLD_ICONV 1
+#endif
+#ifdef __x86_64__
+#define XDL_FAST_HASH 1
+#endif
+#undef GETTEXT_H
+#define GETTEXT_H
+#undef _
+#define _(s) gettext(s)
+#undef N_
+/* N_(x) MUST NOT have any parenthesis around expansion! */
+#define N_(s) s
+#undef Q_
+#define Q_(s,p,n) ngettext((s),(p),(n))
+extern void git_setup_gettext(void);
+extern char *gettext(const char *msgid);
+extern int gettext_width(const char *s);
+#undef ngettext
+#define ngettext(s,p,n) (((n)==1)?gettext(s):gettext(p))
---
+++ src/gettext.c
@@ -0,0 +1,232 @@
+/*
+
+gettext.c - gettext equivalents for Mac OS X
+Copyright (C) 2014 Kyle J. McKay.  All rights reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
+#include "git-compat-util.h"
+#include "gettext.h"
+#include "utf8.h"
+#include <locale.h>
+#include <langinfo.h>
+#include <xlocale.h>
+#include <iconv.h>
+#include <CoreFoundation/CoreFoundation.h>
+
+static char charset[32];
+static char localename[32];
+static int is_utf8;
+static int icok;
+static iconv_t ic;
+static CFBundleRef bndl;
+static CFStringRef localestr;
+static CFDictionaryRef localedict;
+static CFDictionaryRef localeiconvdict;
+
+static char *strlcpyuc(char *dst, const char *src, size_t cnt)
+{
+	char *ptr;
+
+	if (!cnt || !dst)
+		return dst;
+
+	ptr = dst;
+	if (src) {
+		while (*src && --cnt) {
+			ch = *src++;
+			if ('a' <= ch && ch <= 'z')
+				ch -= 'a' + 'A';
+			*ptr++ = ch;
+		}
+	}
+	*ptr = '\0';
+	return dst;
+}
+
+void git_setup_gettext(void)
+{
+	const char *ln, *localedir;
+	char bundlepath[PATH_MAX];
+	CFURLRef burl;
+	CFArrayRef bl;
+
+	setlocale(LC_ALL, "");
+	strlcpyuc(charset, nl_langinfo(CODESET), sizeof(charset));
+	is_utf8 = !strcmp(charset, "UTF-8");
+	strlcpy(localename, querylocale(LC_MESSAGES_MASK, NULL), sizeof(localename));
+	ic = iconv_open(
+		strcmp(charset, "US-ASCII") ? charset : "US-ASCII/TRANSLIT",
+		"UTF-8");
+	if (ic != (iconv_t)-1)
+		icok = 1;
+	localedir = getenv("GIT_TEXTDOMAINDIR");
+	if (!localedir)
+		localedir = GIT_LOCALE_PATH; /* typically $prefix/share/locale */
+	snprintf(bundlepath, sizeof(bundlepath), "%s/git-messages.bundle", localedir);
+	burl = CFURLCreateFromFileSystemRepresentation(kCFAllocatorDefault,
+		(const UInt8 *)bundlepath, strlen(bundlepath), true);
+	if (burl) {
+		bndl = CFBundleCreate(kCFAllocatorDefault, burl);
+		CFRelease(burl);
+	}
+	bl = NULL;
+	if (bndl)
+		bl = CFBundleCopyBundleLocalizations(bndl);
+	if (bl) {
+		CFArrayRef al = NULL;
+		CFBundleRef pl = NULL;
+		CFStringRef l = CFStringCreateWithCString(kCFAllocatorDefault,
+			localename, kCFStringEncodingUTF8);
+		if (l) {
+			pl = CFArrayCreate(kCFAllocatorDefault,
+				(const void **)&l, 1, &kCFTypeArrayCallBacks);
+			CFRelease(l);
+		}
+		if (pl) {
+			al = CFBundleCopyLocalizationsForPreferences(bl, pl);
+			CFRelease(pl);
+		}
+		if (al) {
+			if (CFArrayGetCount(al)) {
+				CFStringRef s = (CFStringRef)CFArrayGetValueAtIndex(al, 0);
+				if (s && CFGetTypeID(s) == CFStringGetTypeID()) {
+				    CFRetain(s);
+				    localestr = s;
+				}
+			}
+			CFRelease(al);
+		}
+		CFRelease(bl);
+	}
+	if (localestr) {
+		CFURLRef lsu = CFBundleCopyResourceURLForLocalization(bndl,
+			CFSTR("Localizable"), CFSTR("strings"), NULL, localestr);
+		if (lsu) {
+			CFDataRef xml;
+			SInt32 err;
+			if (CFURLCreateDataAndPropertiesFromResource(kCFAllocatorDefault,
+			    lsu, &xml, NULL, NULL, &err)) {
+				CFPropertyListRef pl = CFPropertyListCreateFromXMLData(
+					kCFAllocatorDefault, xml, kCFPropertyListImmutable, NULL);
+				CFRelease(xml);
+				if (pl) {
+					if (CFGetTypeID(pl) == CFDictionaryGetTypeID())
+						localedict = pl;
+					else
+						CFRelease(pl);
+				}
+			}
+			CFRelease(lsu);
+		}
+	}
+	if (localedict) {
+		localeiconvdict = CFDictionaryCreateMutable(kCFAllocatorDefault,
+			CFDictionaryGetCount(localedict), &kCFTypeDictionaryKeyCallBacks, NULL);
+	}
+}
+
+int gettext_width(const char *s)
+{
+	return is_utf8 ? utf8_strwidth(s) : strlen(s);
+}
+
+static CFStringRef get_dict_str(CFDictionaryRef d, CFStringRef k)
+{
+	CFStringRef v;
+	if (!d || !k)
+		return NULL;
+	v = (CFStringRef) CFDictionaryGetValue(d, k);
+	return v && CFGetTypeID(v) == CFStringGetTypeID() ? v : NULL;
+}
+
+char *gettext(const char *msgid)
+{
+	CFStringRef key;
+	CFStringRef strval;
+	char *val, *newval;
+	size_t s;
+	int newvalalloc = 0;
+
+	if (!msgid || !*msgid || !localeiconvdict || (!is_utf8 && !icok))
+		return msgid;
+
+	key = CFStringCreateWithCStringNoCopy(kCFAllocatorDefault, msgid,
+		kCFStringEncodingUTF8, NULL);
+	if (!key)
+		return msgid;
+	val = (char *)CFDictionaryGetValue(localeiconvdict, key);
+	if (val) {
+		CFRelease(key);
+		return val;
+	}
+	valstr = get_dict_str(localedict, key);
+	if (!valstr) {
+		CFRelease(key);
+		return msgid;
+	}
+	newval = CFStringGetCStringPtr(valstr, kCFStringEncodingUTF8);
+	if (!newval) {
+		s = (size_t)CFStringGetMaximumSizeForEncoding(CFStringGetLength(valstr),
+			kCFStringEncodingUTF8) + 1;
+		newval = (char *)malloc(s);
+		if (!newval) {
+			CFRelease(key);
+			return msgid;
+		}
+		if (!CFStringGetCString(valstr, newval, (CFIndex)s, kCFStringEncodingUTF8)) {
+			free(newval);
+			CFRelease(key);
+			return msgid;
+		}
+		newvalalloc = 1;
+	}
+	if (is_utf8) {
+		/* no iconv needed */
+		CFDictionarySetValue(localeiconvdict, key, newval);
+		CFRelease(key);
+		return newval;
+	}
+	{
+		size_t iclen = strlen(newval);
+		char *newival = (char *)malloc(iclen + 8);
+		size_t ilen, olen;
+		char *iptr, *optr;
+
+		if (!newival) {
+			if (newvalalloc) free(newval);
+			CFRelease(key);
+			return msgid;
+		}
+		iconv(ic, NULL, NULL, NULL, NULL);
+		iptr = newval;
+		optr = newival;
+		ilen = iclen;
+		olen = iclen + 7;
+		if (iconv(ic, (const char **)&newval, &ilen, &newival, &olen) != (size_t)-1) {
+			*optr = '\0';
+			CFDictionarySetValue(localeiconvdict, key, newival);
+			if (newvalalloc) free(newval);
+			CFRelease(key);
+			return newival;
+		}
+		free(newival);
+		if (newvalalloc) free(newval);
+		CFRelease(key);
+	}
+	return msgid;
+}

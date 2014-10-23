938f2de14f45c9fab88b699915c2d9883335b044
Kyle J. McKay <mackyle@gmail.com>
2014-10-22 17:13:54 -0700

    implement the last of the compatibility functions
    It works!  Yeah!  A Tiger build with git-imap-send imaps support
    and no links to OpenSSL!  Including CRAM-MD5 support in imap
    tunnels as well!

--- include/CommonCrypto/CommonHMAC.h
+++ include/CommonCrypto/CommonHMAC.h
@@ -8,7 +8,7 @@
 
 typedef struct {
 	CC_MD5_CTX md5ctx;
-	unsigned char keydata[64];
+	unsigned char keydata[CC_MD5_BLOCK_BYTES];
 } CCHmacContext;
 
 void cCCHmacInit(CCHmacContext *,int,const void *,size_t);
@@ -18,11 +18,5 @@ void cCCHmacFinal(CCHmacContext *, void *);
 #define CCHmacInit(c,a,k,l) cCCHmacInit(c,a,k,l)
 #define CCHmacUpdate(c,d,l) cCCHmacUpdate(c,d,l)
 #define CCHmacFinal(c,m) cCCHmacFinal(c,m)
-
-int cEVP_EncodeBlock(void *,const void *,int);
-int cEVP_DecodeBlock(void *,const void *,int);
-
-#define EVP_EncodeBlock(t,f,l) cEVP_EncodeBlock(t,f,l)
-#define EVP_DecodeBlock(t,f,l) cEVP_DecodeBlock(t,f,l)
 
 #endif /* _CC_COMMON_HMAC_H_ */
